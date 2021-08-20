05234d6e8bbae11673b53d21510ddd9de5b781d5
RealNattawattHongthong <73651838+RealNattawattHongthong@users.noreply.github.com>
2021-08-20 08:06:52 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ print("Make By RealNattawattHo")
 time.sleep(3)
 print("contact me at https://treenattawatt.web.app/")
 time.sleep(3)
-print("if you want to end process the program press q to stop the program")
+print("if you want to end process the program press k to stop the program")
 time.sleep(2)
 wait = input("press enter to continue..")
 time.sleep(5)
-
 mouse = pyautogui.position()
 while True:
     pyautogui.click(mouse)
-    if keyboard.is_pressed('q'):
+    if keyboard.is_pressed('k'):
         break
